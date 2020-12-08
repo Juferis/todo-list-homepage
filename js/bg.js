2821840a48c825749b6e10f16a5f89b7f11f6e28
Juferis <66511872+Juferis@users.noreply.github.com>
2020-12-08 11:39:32 +0900

    update BG images

--- js/bg.js
+++ js/bg.js
@@ -1,3 +1,4 @@
+/* Random background
 const body = document.querySelector("body");
 
 const IMG_NUMBER = 5;
@@ -17,5 +18,55 @@ function genRandom() {
 function init() {
   const randomNumber = genRandom();
   paintImage(randomNumber);
+}
+init();
+*/
+const body = document.querySelector("body");
+const theme1 = document.querySelector(".theme_1");
+const theme2 = document.querySelector(".theme_2");
+const theme3 = document.querySelector(".theme_3");
+const theme4 = document.querySelector(".theme_4");
+
+const basicBG = "1";
+const BG_LS = "BackGround";
+
+function removeBackGround() {
+  localStorage.removeItem(BG_LS);
+}
+function saveBackGround(themeNum) {
+  localStorage.setItem(BG_LS, themeNum);
+}
+function paintImage(themeNum) {
+  const image = new Image();
+  image.src = `images/${themeNum}.jpg`;
+  image.classList.add("bgImage");
+  body.appendChild(image);
+}
+function setBackGround(event) {
+  const themeNum = event.target.parentNode.value;
+  if (themeNum === undefined) {
+    console.log("aaaa");
+    themeNum = basicBG;
+  }
+  paintImage(themeNum);
+  removeBackGround();
+  saveBackGround(themeNum);
+  location.reload();
+}
+function paintBackground() {
+  const bgNum = localStorage.getItem(BG_LS);
+  if (bgNum === null) {
+    localStorage.setItem(BG_LS, basicBG);
+    paintImage(basicBG);
+  } else {
+    paintImage(bgNum);
+  }
+}
+function init() {
+  paintBackground();
+  theme1.addEventListener("click", setBackGround);
+  theme2.addEventListener("click", setBackGround);
+  theme3.addEventListener("click", setBackGround);
+  theme4.addEventListener("click", setBackGround);
 }
 init();
